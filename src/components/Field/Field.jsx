46dda7332c16535797e148a3b167b7bf965ef06f
dmi3mid <dmi3midd@gmail.com>
2025-04-02 13:48:26 +0300

    Fix

--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -1,11 +1,11 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 // import axios from 'axios';
 
 import useGame from '../../hooks/useGame';
 import useTelegram from '../../hooks/useTelegram';
 
 import Button from '../UI/Button/Button';
-import SendDataButton from '../UI/Button/SendDataButton';
+// import SendDataButton from '../UI/Button/SendDataButton';
 
 import classes from './Field.module.css';
 
@@ -21,16 +21,19 @@ export default function Field() {
     //     const sentData = await axios.get(`https://api.telegram.org/bot${}/sendMessage?chat_id=${chat?.id}&text=Привіт%2C+це+повідомлення+з+бота%21`)
     // }
 
-    useEffect(() => {
-        WebAppMainButton.setText(`Your count: ${playerCount}`);
-        WebAppMainButton.show();
-    }, [WebAppMainButton, playerCount]);
+    // useEffect(() => {
+    //     WebAppMainButton.setText(`Your count: ${playerCount}`);
+    //     WebAppMainButton.show();
+    // }, [WebAppMainButton, playerCount]);
     // const onSendData = useCallback(() => {
     //     tg.sendData(JSON.stringify({playerCount, user}));
     // }, [tg, user, playerCount]);
     const onSendData = () => {
         tg.sendData(JSON.stringify({playerCount, user}));
     };
+    WebAppMainButton.setText(`Your count: ${playerCount}`);
+    WebAppMainButton.show();
+    WebAppMainButton.onClick(onSendData);
     // useEffect(() => {
     //     tg.onEvent('mainButtonClicked', onSendData);
     //     return () => {
@@ -61,9 +64,9 @@ export default function Field() {
                 <Button onMove={onMove} playerMove="Shears">
                     <img className={classes.buttonImg} src={require("../../images/Shears.webp")} alt="" />
                 </Button>
-                <SendDataButton onSendData={onSendData}>
+                {/* <SendDataButton onSendData={onSendData}>
                     <img className={classes.buttonImg} src={require("../../images/Door.webp")} alt="" />
-                </SendDataButton>
+                </SendDataButton> */}
             </div>
         </div>
     )
